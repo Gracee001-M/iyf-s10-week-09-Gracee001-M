8ce81f1d442522d8d87f93888271bfd7c641a683
Gracee001-M <gracemacharia031@gmail.com>
2026-04-28 18:30:44 +0300

    Add main.jsx

--- App.jsx
+++ App.jsx
@@ -204,3 +204,27 @@ function PostList() {
         </div>
     );
 }
+
+// App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="posts" element={<Posts />} />
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
+}
+
+// App.jsx
+<Route path="posts/:postId" element={<PostDetail />} />
---
+++ main.jsx
@@ -0,0 +1,9 @@
+// main.jsx
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
