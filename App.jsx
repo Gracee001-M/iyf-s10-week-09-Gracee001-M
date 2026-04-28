a2da91cd7367222f92f10ee5e7f53663c0ee55c8
Gracee001-M <gracemacharia031@gmail.com>
2026-04-28 19:11:03 +0300

    Add Mini-Project: Complete CommunityHub Frontend

--- App.jsx
+++ App.jsx
@@ -254,3 +254,27 @@ function Navigation() {
         </nav>
     );
 }
+
+// App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import PostDetail from './pages/PostDetail';
+import CreatePost from './pages/CreatePost';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path="posts/:postId" element={<PostDetail />} />
+        <Route path="create" element={<CreatePost />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  );
+}
+export default App;
--- main.jsx
+++ main.jsx
@@ -2,8 +2,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
     </BrowserRouter>
 );
+
+
