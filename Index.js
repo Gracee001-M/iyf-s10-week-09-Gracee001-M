a465c41241ef7789dd61ac71cbc04ca43b7d69e9
Gracee001-M <gracemacharia031@gmail.com>
2026-04-28 19:04:06 +0300

    Add Index.js

---
+++ Index.js
@@ -0,0 +1,6 @@
+// components/shared/index.js
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Card } from './Card';
+export { default as Modal } from './Modal';
+export { default as Avatar } from './Avatar';
